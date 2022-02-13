fdc1ba07408702d74e8b7f8b6aa450efd50f1e41
Andy Gallegos <andysgallegos@gmail.com>
2022-02-13 02:47:05 +0000

    Update s1_custom_attribute.ps1

--- s1_custom_attribute.ps1
+++ s1_custom_attribute.ps1
@@ -2,6 +2,7 @@
 #check if new attribute equals the old attribute
 #if yes, write "attribute already exists)
 #if no, set new attribute
+#also check perform same process for directory information
 
 $new_attribute="google.com"
 $current_attribute=(get-mailbox upn).customattribute1
@@ -13,5 +14,3 @@ if ($new_attribute -eq $current_attribute) {
     $attribute=(get-mailbox upn).customattribute1
     write-host "Attribute has been updated to $attribute"
     }
-
-	
