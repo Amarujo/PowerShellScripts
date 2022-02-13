ca7af42bcd4c9e9e17428ff16bfe74b0ae04fc2b
Andy Gallegos <andysgallegos@gmail.com>
2022-02-13 02:45:17 +0000

    Update s1_custom_attribute.ps1

--- s1_custom_attribute.ps1
+++ s1_custom_attribute.ps1
@@ -4,14 +4,14 @@
 #if no, set new attribute
 
 $new_attribute="google.com"
-$current_attribute=(get-mailbox LynneR@amarujo.onmicrosoft.com).customattribute1
+$current_attribute=(get-mailbox upn).customattribute1
 if ($new_attribute -eq $current_attribute) { 
     write-host "New and current attribute are the same, no changes made."
-    } 
-	else {
-	Set-Mailbox LynneR@amarujo.onmicrosoft.com -CustomAttribute1 $new_attribute 
-	$attribute=(get-mailbox LynneR@amarujo.onmicrosoft.com).customattribute1
-	write-host "Attribute has been updated to $attribute"
-	}
+    }
+    else {
+    Set-Mailbox LynneR@amarujo.onmicrosoft.com -CustomAttribute1 $new_attribute 
+    $attribute=(get-mailbox LynneR@amarujo.onmicrosoft.com).customattribute1
+    write-host "Attribute has been updated to $attribute"
+    }
 
 	
