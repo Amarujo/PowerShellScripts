ef3a40c7061ffe42e9ec822c90662820155d43cb
Andy Gallegos <andysgallegos@gmail.com>
2022-02-13 02:35:52 +0000

    Update s1_custom_attribute.ps1

--- s1_custom_attribute.ps1
+++ s1_custom_attribute.ps1
@@ -1,15 +1,15 @@
-#for each user in csv file
-#check if new attribute equals the old attribute
-#if yes, write "attribute already exists)
-#if no, set new attribute
+#Users will periodically need their custom attributes updated.
+#Will eventually expand this to go through a csv, check multiple columns for multiple properties (directory info, phone number, etc) 
+#and make sure it matches or update as necessary
 
-$new_attribute="google.com"
-$old_attribute=(get-mailbox andy.gallegos@amarujo.onmicrosoft.com).customattribute1
+
+$new_attribute="new website"
+$old_attribute=(get-mailbox ups).customattribute1
 if ($new_attribute -eq $current_attribute) { 
     write-host "New and current attribute are the same, no changes made."
     } 
 	else {
-	Set-Mailbox andy.gallegos@amarujo.onmicrosoft.com -CustomAttribute1 $new_attribute
+	Set-Mailbox ups -CustomAttribute1 $new_attribute
 	}
 
 	
