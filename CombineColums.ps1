0b62f02e2191b9c97c61cc2c106fe854bf4dec46
Andy Gallegos <andysgallegos@gmail.com>
2022-10-20 11:49:35 -0400

    Update CombineColums.ps1

--- CombineColums.ps1
+++ CombineColums.ps1
@@ -1,6 +1,6 @@
 #Run this one liner to create a new csv file with the output of two combined columns from an original csv.  Need to find a way to add this column to original csv.
 
-import-csv .\sourcecsv |select-object @{name='column name you want';expression={$_.column1,$_.column1 -join ' ' }} | export-csv combinedcsv.csv -notypeinformation
+import-csv .\sourcecsv |select-object @{name='column name you want';expression={$_.column1,$_.column1 -join 'add separator' }} | export-csv combinedcsv.csv -notypeinformation
 
 #example:
 
@@ -8,8 +8,8 @@ import-csv .\sourcecsv |select-object @{name='column name you want';expression={
 # | firstname | lastname |
 # | andy      | doe      |
 
-# import-csv .\sourcecsv |select-object @{name='fullname';expression={$_.firstname,$_.lastname -join ' ' }} | export-csv combinedcsv.csv -notypeinformation
+# import-csv .\sourcecsv |select-object @{name='fullname';expression={$_.firstname,$_.lastname -join '-' }} | export-csv combinedcsv.csv -notypeinformation
 
 # combined.csv 
 # | displayname |
-# | andy doe    |
+# | andy-doe    |
